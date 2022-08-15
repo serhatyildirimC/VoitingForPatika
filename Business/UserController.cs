7e830d39557b5723ed8251dc3970197c2d9ed63b
yilldirimserhat@gmail.com <yilldirimserhat@gmail.com>
2022-08-15 16:47:22 +0300

    Add project files.

---
+++ Business/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Data;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class UserController
+    {
+        InMemory inMemory = new InMemory();
+
+        public bool Controller(string UserName,string Password)
+        {
+           User user = inMemory.Get(UserName);
+
+            if (user.Password == Password)
+            {
+                return true;
+            }
+            else return false;
+                  
+        }
+        public bool AddUser(string UserName,string PassWord)
+        {
+            int i = 0;  
+            List<User> users = inMemory.GetUsers();
+           foreach(User user in users)
+            {
+                if (user.UserName == UserName)
+                {
+                    i++;
+                }
+            }
+           if (i == 0)
+            {
+                return false;
+            }
+           else
+            {
+                User user = new User { Id = users.Count+1, UserName = UserName, Password = PassWord } ;
+                inMemory.Add(user);
+                return true;
+            }
+
+        }
+    }
+}
---
+++ Voiting/Program.cs
@@ -0,0 +1,41 @@
+﻿using Business;
+
+List<string> catagories = new List<string> { "macera", "aksiyon", "komedi", "romantik" };
+Console.WriteLine("Hoşgeldiniz kullanıcı adınızı giriniz:");
+string userName = Console.ReadLine();
+Console.WriteLine("Şifre giriniz:");
+string password = Console.ReadLine();
+UserController userController = new UserController();
+bool a = userController.Controller(userName, password);
+int maceraCount = 0;
+int aksiyonCount = 0;
+int komediCount = 0;
+int romantikCount = 0;
+
+if (a== true)
+{
+    Console.WriteLine("giriş onaylandı hoşgeldiniz");
+    Console.WriteLine("lütfen oylamaya katılın");
+    for (int i = 0  ; i < catagories.Count; i++)
+    {
+        Console.WriteLine(catagories[i]+" için "+ i);
+    }
+    Console.WriteLine("oyunuz için teşekkürler.");
+    int b = Convert.ToInt32(Console.ReadLine());
+    
+    if (b == 1) maceraCount++;
+    if (b == 2) aksiyonCount++;
+    if (b == 3) komediCount++;
+    if (b == 4) romantikCount++;
+    
+}
+if (a== false)
+{
+    Console.WriteLine("kayıt olmak için username giriniz");
+    string newUserName = Console.ReadLine();
+    Console.WriteLine("şifre girin");
+    string newPassword = Console.ReadLine();
+    bool c = userController.AddUser(newUserName, newPassword);
+    if (c == true) Console.WriteLine("kayıt başarılı");
+    else Console.WriteLine("kullanıcı adı kullanımda");    
+}
